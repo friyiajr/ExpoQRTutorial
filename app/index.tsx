d11ba590a279990870d93fbf5a8163907114bb37
Daniel Friyia <dfriyia@gmail.com>
2024-08-17 11:39:13 -0400

    Solution

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,34 @@
 import { View, Text, StyleSheet, SafeAreaView, Pressable } from "react-native";
 import { Link, Stack } from "expo-router";
 
+import { useCameraPermissions } from "expo-camera";
+
 export default function Home() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  const isPermissionGranted = Boolean(permission?.granted);
+
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ title: "Overview", headerShown: false }} />
       <Text style={styles.title}>QR Code Scanner</Text>
-      <View style={{ gap: 20 }}></View>
+      <View style={{ gap: 20 }}>
+        <Pressable onPress={requestPermission}>
+          <Text style={styles.buttonStyle}>Request Permissions</Text>
+        </Pressable>
+        <Link href={"/scanner"} asChild>
+          <Pressable disabled={!isPermissionGranted}>
+            <Text
+              style={[
+                styles.buttonStyle,
+                { opacity: !isPermissionGranted ? 0.5 : 1 },
+              ]}
+            >
+              Scan Code
+            </Text>
+          </Pressable>
+        </Link>
+      </View>
     </SafeAreaView>
   );
 }
--- app/scanner/index.tsx
+++ app/scanner/index.tsx
@@ -1,7 +1,36 @@
+import { Camera, CameraView } from "expo-camera";
 import { Stack } from "expo-router";
-import { SafeAreaView, StyleSheet } from "react-native";
+import {
+  AppState,
+  Linking,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import { Overlay } from "./Overlay";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const qrLock = useRef(false);
+  const appState = useRef(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        qrLock.current = false;
+      }
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
     <SafeAreaView style={StyleSheet.absoluteFillObject}>
       <Stack.Screen
@@ -10,6 +39,20 @@ export default function Home() {
           headerShown: false,
         }}
       />
+      {Platform.OS === "android" ? <StatusBar hidden /> : null}
+      <CameraView
+        style={StyleSheet.absoluteFillObject}
+        facing="back"
+        onBarcodeScanned={({ data }) => {
+          if (data && !qrLock.current) {
+            qrLock.current = true;
+            setTimeout(async () => {
+              await Linking.openURL(data);
+            }, 500);
+          }
+        }}
+      />
+      <Overlay />
     </SafeAreaView>
   );
 }
